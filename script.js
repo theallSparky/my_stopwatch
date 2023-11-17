e0063abe8b10f6ccff0e7c65f53d612045171a5d
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-11-17 11:49:05 -0600

    Created functionality for the start button

---
+++ script.js
@@ -0,0 +1,58 @@
+window.onload = function() {
+    //Initial values for stopwatch upon loading
+    let minutes = 0
+    let seconds = 0
+    let tens = 0
+
+    let appendMinutes = document.querySelector('#minutes')
+    let appendSeconds = document.querySelector('#seconds')
+    let appendTens = document.querySelector('#tens')
+
+    let startBtn = document.querySelector('#start')
+    let stopBtn = document.querySelector('#stop')
+    let resetBtn = document.querySelector('#reset')
+
+    let Interval
+
+    const startTimer = () => {
+        tens++
+        if(tens <= 9) {
+            appendTens.innerHTML = '0' + tens
+        }
+        if (tens > 9) {
+            appendTens.innerHTML = tens
+        }
+
+        if (tens > 99) {
+            seconds++
+            appendSeconds.innerHTML = '0' + seconds
+            tens = 0
+            appendTens.innerHTML = '0' + 0
+        }
+
+        if (seconds > 9) {
+            appendSeconds.innerHTML = seconds
+        }
+
+        if (seconds > 59) {
+            minutes++
+            appendMinutes.innerHTML = '0' + minutes
+            seconds= 0
+            appendSeconds.innerHTML = '0' + 0
+        }
+    }
+
+
+    startBtn.onclick = () => {
+        clearInterval(Interval)
+        Interval = setInterval(startTimer, 10)
+    }
+
+
+
+
+
+
+
+
+}
